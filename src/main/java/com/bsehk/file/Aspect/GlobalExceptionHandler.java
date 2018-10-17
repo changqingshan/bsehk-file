e8daffd7ef4de00130f93663f5c5162aba7c987c
wei <changminwei@synjones.net>
2018-10-17 09:31:55 +0800

    初始化

---
+++ src/main/java/com/bsehk/file/Aspect/GlobalExceptionHandler.java
@@ -0,0 +1,23 @@
+package com.bsehk.file.Aspect;
+
+import com.bsehk.common.response.ResultData;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+/**
+ * Created by 24398 on 2018/9/4.
+ */
+
+@RestControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResultData exceptionHandler(Exception e){
+
+        log.error("全局异常处理 :  [{}]",e);
+        return ResultData.fail(e.getMessage());
+    }
+
+}
---
+++ src/main/java/com/bsehk/file/FileServiceApplication.java
@@ -0,0 +1,14 @@
+package com.bsehk.file;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
+@SpringBootApplication
+public class FileServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileServiceApplication.class, args);
+	}
+}
